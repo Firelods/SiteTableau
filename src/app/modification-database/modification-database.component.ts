8d7ac009cd03d528c7271b25457e9d157db2a9c6
Firelods <35725423+Firelods@users.noreply.github.com>
2022-06-08 00:09:19 +0200

    a

--- src/app/modification-database/modification-database.component.ts
+++ src/app/modification-database/modification-database.component.ts
@@ -1,8 +1,15 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Photo } from '../photo';
 import { PhotoService } from '../photo.service';
-
+const httpOptions : any    = {
+  headers: new HttpHeaders({
+    //'Content-Type':  'application/json',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'GET',
+    'Access-Control-Allow-Origin': '*'
+  })
+};
 @Component({
   selector: 'app-modification-database',
   templateUrl: './modification-database.component.html',
@@ -10,21 +17,39 @@ import { PhotoService } from '../photo.service';
 })
 export class ModificationDatabaseComponent implements OnInit {
   photo: Photo={auteur:"",titre:"",dimension:"",categorie:"",prix:0,idINT:0};
+  imageToSend: string = "";
+  formData: any;
   private photoURL="http://db.clement-lefevre.fr"
   
   constructor(private http:HttpClient,private photoService:PhotoService) { }
 
   ngOnInit(): void {
+    this.photoService.getNbPhotos().subscribe(nb => {this.photo.idINT=nb+1;console.log(nb)});
   }
   onEnterAuteur(auteur: string) { this.photo.auteur = auteur; }
   onEnterTitre(titre: string) { this.photo.titre = titre; }
   onEnterDimension(dimension: string) { this.photo.dimension = dimension; }
   onEnterCategorie(categorie: string) { this.photo.categorie = categorie; }
   onEnterPrix(prix: string) { this.photo.prix = parseInt(prix); }
+  
+
+  setImageToSend(event: any){
+    const file:File = event.target.files[0];
+    console.log(file);
+    this.formData = new FormData();
+    console.log(this.formData);
+    this.formData.append("imgSent", file);
+    console.log(this.formData.getAll("imgSent"));
+    
+    
+    
+  }
+
   ajouterArticle(): void {
+    // this.formData.append("titre", this.photo.titre);
+    this.http.post(this.photoURL+"/addImage", this.formData).subscribe(tableau => console.log(tableau));
     console.log(this.photo);
-    this.photoService.getNbPhotos().subscribe(nb => {this.photo.idINT=nb+1;});
-    this.http.post<Photo>(this.photoURL+"/add",this.photo).subscribe(tableau => console.log(tableau));
+    // this.http.post<Photo>(this.photoURL+"/add",this.photo).subscribe(tableau => console.log(tableau));
     window.alert("Photo added successfully");
   }
 }
